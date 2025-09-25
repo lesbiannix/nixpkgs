4a0bf9d6b3072b4dfe80f76bc51eb3f8abeafaf0
Yureka <yureka@cyberchaos.dev>
2025-09-25 11:20:32 +0200

    vial: build from source

--- pkgs/by-name/vi/vial/package.nix
+++ pkgs/by-name/vi/vial/package.nix
@@ -1,37 +1,67 @@
 {
   lib,
-  fetchurl,
-  appimageTools,
+  fetchFromGitHub,
+  python3Packages,
+  qt6,
 }:
-let
-  version = "0.7.5";
-  pname = "Vial";
 
-  src = fetchurl {
-    url = "https://github.com/vial-kb/vial-gui/releases/download/v${version}/${pname}-v${version}-x86_64.AppImage";
-    hash = "sha256-sN8i/MOPhaLZ4iJNKz/MdpRIGTZVV/G5qD7o+ID8dAM=";
+python3Packages.buildPythonPackage rec {
+  pname = "vial-gui";
+  version = "0.7.5";
+
+  src = fetchFromGitHub {
+    owner = "vial-kb";
+    repo = "vial-gui";
+    tag = "v${version}";
+    hash = "sha256-TWcm+UgROpd5pX/EV0SMx52C9i9Ip9vT61OQhsTiRi8=";
   };
 
-  appimageContents = appimageTools.extractType2 { inherit pname version src; };
-in
-appimageTools.wrapType2 {
-  inherit pname version src;
+  format = "other";
 
-  extraInstallCommands = ''
-    install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
-    cp -r ${appimageContents}/usr/share/icons $out/share
+  nativeBuildInputs = [
+    qt6.wrapQtAppsHook
+  ];
+
+  buildInputs = [
+    qt6.qtbase
+  ];
+
+  dependencies = with python3Packages; [
+    fbs
+    certifi
+    pyqt5
+    simpleeval
+    hidapi
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin $out/libexec
+    cp -r . $out/libexec/vial
+
+    sed -i '1i#!/usr/bin/env python' $out/libexec/vial/src/main/python/main.py
+    chmod u+x $out/libexec/vial/src/main/python/main.py
+    ln -s $out/libexec/vial/src/main/python/main.py $out/bin/vial-gui
 
     mkdir -p $out/etc/udev/rules.d/ # https://get.vial.today/getting-started/linux-udev.html
     echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0666", TAG+="uaccess", TAG+="udev-acl"' > $out/etc/udev/rules.d/92-viia.rules
+
+    runHook postInstall
+  '';
+
+  makeWrapperArgs = [ "--chdir $out/libexec/vial" ];
+
+  postFixup = ''
+    wrapPythonProgramsIn "$out/libexec/vial/src/main/python" "$out $pythonPath"
   '';
 
   meta = {
     description = "Open-source GUI and QMK fork for configuring your keyboard in real time";
     homepage = "https://get.vial.today";
     license = lib.licenses.gpl2Plus;
-    mainProgram = "Vial";
+    mainProgram = "vial-gui";
     maintainers = with lib.maintainers; [ kranzes ];
-    platforms = [ "x86_64-linux" ];
-    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    platforms = lib.platforms.linux;
   };
 }
